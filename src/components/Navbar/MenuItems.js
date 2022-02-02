67b803d352aea0f009068ce6741a7671ef946242
John Morgan <jwjmorgan@gmail.com>
2022-02-02 16:00:01 +0000

    Add MenuItems.js and map items to nav

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,5 @@
+export const MenuItems = [
+  { title: "About", url: "#" },
+  { title: "Projects", url: "#" },
+  { title: "Contact", url: "#" },
+];
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,20 +1,17 @@
 import React from "react";
 import classes from "./Navbar.module.css";
+import { MenuItems } from "./MenuItems";
 
 const Navbar = () => {
   return (
     <nav className={classes.navbar}>
       <h1 className={classes.navbar__logo}>JohnMorgan.dev</h1>
       <ul>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Projects</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
+        {MenuItems.map((item, index) => (
+          <li key={index}>
+            <a href={item.url}>{item.title}</a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
