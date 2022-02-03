74edf51cc68eb505f72c9b805733652330f8b0ed
John Morgan <jwjmorgan@gmail.com>
2022-02-03 00:28:10 +0000

    Create Hamburger component

---
+++ src/components/Navbar/Hamburger/Hamburger.js
@@ -0,0 +1,21 @@
+import React from "react";
+import classes from "../Navbar.module.css";
+
+const Hamburger = ({ menuOpen, setMenuOpen }) => {
+  return (
+    <div className={classes.navbar__hamburger}>
+      <div
+        onClick={() => setMenuOpen(!menuOpen)}
+        className={`${classes["navbar__hamburger-icon"]} ${
+          menuOpen && classes.open
+        }`}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default Hamburger;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import NavLinks from "./NavLinks/NavLinks";
+import Hamburger from "./Hamburger/Hamburger";
 import classes from "./Navbar.module.css";
 
 const Navbar = () => {
@@ -9,18 +10,7 @@ const Navbar = () => {
     <nav className={classes.navbar}>
       <h1 className={classes.navbar__logo}>JohnMorgan.dev</h1>
       <NavLinks menuOpen={menuOpen} />
-      <div className={classes.navbar__hamburger}>
-        <div
-          onClick={() => setMenuOpen(!menuOpen)}
-          className={`${classes["navbar__hamburger-icon"]} ${
-            menuOpen && classes.open
-          }`}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
+      <Hamburger menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
     </nav>
   );
 };
