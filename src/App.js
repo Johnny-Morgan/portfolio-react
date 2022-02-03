691dee0e8abf72f99722fd96b845b08e5982d178
John Morgan <jwjmorgan@gmail.com>
2022-02-03 11:19:24 +0000

    Add components to App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import Navbar from "./components/Navbar/Navbar";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
 
 function App() {
-  return <Navbar />;
+  return (
+    <div className="app">
+      <Navbar />
+      <div className="sections">
+        <About />
+        <Projects />
+        <Contact />
+      </div>
+    </div>
+  );
 }
 
 export default App;
