21cbf164529646fbecaec2b38940b3084d9f1658
John Morgan <jwjmorgan@gmail.com>
2022-02-07 13:43:33 +0000

    Add projects data file

---
+++ src/components/Projects/ProjectItems.js
@@ -0,0 +1,17 @@
+export const ProjectItems = [
+  {
+    title: "Pickles",
+    liveURL: "https://pickles-eshop.herokuapp.com/",
+    repoURL: "https://github.com/Johnny-Morgan/pickles",
+  },
+  {
+    title: "Hike Logger",
+    liveURL: "https://hike-logger.herokuapp.com/",
+    repoURL: "https://github.com/Johnny-Morgan/hike-logger",
+  },
+  {
+    title: "Memory Blocks",
+    liveURL: "https://johnny-morgan.github.io/memory-blocks/",
+    repoURL: "https://github.com/Johnny-Morgan/memory-blocks",
+  },
+];
