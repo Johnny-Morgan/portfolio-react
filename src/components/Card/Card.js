2ead905d960b2f0e48814f77598ecd78e0c870d6
John Morgan <jwjmorgan@gmail.com>
2022-02-07 13:56:42 +0000

    Add props to Card component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,23 +2,19 @@ import React from "react";
 import classes from "./Card.module.css";
 import image from "./test.png";
 
-const Card = () => {
+const Card = ({ title, about, liveURL, repoURL }) => {
   return (
     <div className={classes.card}>
       <img className={classes.card__img} src={image} alt="test image" />
       <div className={classes.card__content}>
-        <h2>Pickles</h2>
-        <p>An e-commerce website built with the Django Framework.</p>
+        <h2>{title}</h2>
+        <p>{about}</p>
         <div className={classes["card__content--btns"]}>
-          <a
-            href="https://pickles-eshop.herokuapp.com/"
-            target="_blank"
-            className={classes.btn}
-          >
+          <a href={liveURL} target="_blank" className={classes.btn}>
             Live Site
           </a>
           <a
-            href="https://github.com/Johnny-Morgan/pickles"
+            href={repoURL}
             target="_blank"
             className={`${classes.btn} ${classes["btn--outline"]}`}
           >
