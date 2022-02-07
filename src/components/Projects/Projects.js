fd0352cebeb12eb0ce36e3d85eae1d8d7331b837
John Morgan <jwjmorgan@gmail.com>
2022-02-07 13:57:26 +0000

    Map through ProjectItems to display projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Card from "../Card/Card";
 import classes from "./Projects.module.css";
+import { ProjectItems } from "./ProjectItems";
 
 const Projects = () => {
   return (
     <section id="projects" className={classes.projects}>
       <div className={classes.container}>
-        <Card />
-        <Card />
-        <Card />
+        {ProjectItems.map((project) => (
+          <Card
+            title={project.title}
+            about={project.about}
+            liveURL={project.liveURL}
+            repoURL={project.repoURL}
+          />
+        ))}
       </div>
     </section>
   );
