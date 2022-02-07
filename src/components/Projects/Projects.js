96b1f82813c983ceecd05a59044b01943bab83c3
John Morgan <jwjmorgan@gmail.com>
2022-02-07 11:42:32 +0000

    Create Card component

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Card from "../Card/Card";
 import classes from "./Projects.module.css";
 
 const Projects = () => {
-  return <div id="projects" className={classes.projects}></div>;
+  return (
+    <div id="projects" className={classes.projects}>
+      <Card />
+    </div>
+  );
 };
 
 export default Projects;
