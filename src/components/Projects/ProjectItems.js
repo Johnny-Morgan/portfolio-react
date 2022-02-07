c5e9cf150f4e1d5288fa84ff3f86053be6164271
John Morgan <jwjmorgan@gmail.com>
2022-02-07 13:55:06 +0000

    Add about field to project data

--- src/components/Projects/ProjectItems.js
+++ src/components/Projects/ProjectItems.js
@@ -1,16 +1,19 @@
 export const ProjectItems = [
   {
     title: "Pickles",
+    about: "An e-commerce website built with the Django Framework.",
     liveURL: "https://pickles-eshop.herokuapp.com/",
     repoURL: "https://github.com/Johnny-Morgan/pickles",
   },
   {
     title: "Hike Logger",
+    about: "A fun and challenging memory game.",
     liveURL: "https://hike-logger.herokuapp.com/",
     repoURL: "https://github.com/Johnny-Morgan/hike-logger",
   },
   {
     title: "Memory Blocks",
+    about: "The perfect place to log and share your hikes.",
     liveURL: "https://johnny-morgan.github.io/memory-blocks/",
     repoURL: "https://github.com/Johnny-Morgan/memory-blocks",
   },
