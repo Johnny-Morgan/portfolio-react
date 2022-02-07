def6563556eb39a8b5875e5f46120d66624f65fd
John Morgan <jwjmorgan@gmail.com>
2022-02-07 11:48:19 +0000

    Add test image

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,13 @@
 import React from "react";
 import classes from "./Card.module.css";
+import image from "./test.png";
 
 const Card = () => {
-  return <div className={classes.card}>Card</div>;
+  return (
+    <div className={classes.card}>
+      <img src={image} alt="test image" />
+    </div>
+  );
 };
 
 export default Card;
