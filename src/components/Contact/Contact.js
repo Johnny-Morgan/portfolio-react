683ac12112ad6fcd19e5b94fd711960b7262c544
John Morgan <jwjmorgan@gmail.com>
2022-02-09 12:28:24 +0000

    Add header and form to contact section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,22 @@
 import React from "react";
 import classes from "./Contact.module.css";
 const Contact = () => {
-  return <div id="contact" className={classes.contact}></div>;
+  return (
+    <section id="contact" className={classes.contact}>
+      <h2 className={classes.header}>Contact</h2>
+      <div className={classes.container}>
+        <form className="form">
+          <label htmlFor="name">Name</label>
+          <input type="text" name="name" id="name" />
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" />
+          <label htmlFor="message">Message</label>
+          <textarea name="message" id="message" cols="30" rows="10"></textarea>
+          <button>Submit</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
