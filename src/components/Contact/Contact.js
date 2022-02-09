ab6131d96d0bcc9d529bb22710dc5bc039e017c6
John Morgan <jwjmorgan@gmail.com>
2022-02-09 16:35:30 +0000

    Add EmailJS functionality

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Button from "../UI/Button/Button";
 import classes from "./Contact.module.css";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const [confirmationMessage, setConfirmationMessage] = useState(false);
+  const formRef = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ra2u0qi",
+        "template_flofr4r",
+        formRef.current,
+        "user_PLQesPVLRIyOZCs7SPhzI"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
     setConfirmationMessage(true);
+    // Reset form
+    formRef.current.name.value = "";
+    formRef.current.email.value = "";
+    formRef.current.message.value = "";
   };
 
   return (
@@ -18,13 +41,19 @@ const Contact = () => {
           Interested in working together? Get in touch by filling out the form
           below.
         </p>
-        <form className={classes.form} onSubmit={handleSubmit}>
+        <form ref={formRef} className={classes.form} onSubmit={handleSubmit}>
           <label htmlFor="name">Name</label>
-          <input type="text" name="name" id="name" />
+          <input type="text" name="name" id="name" required />
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input type="email" name="email" id="email" required />
           <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" cols="30" rows="10"></textarea>
+          <textarea
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+            required
+          ></textarea>
           <Button type="submit">Submit</Button>
         </form>
         {confirmationMessage && (
