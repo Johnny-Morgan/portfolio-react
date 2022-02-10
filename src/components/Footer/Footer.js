cf7167964f04df163bb2c110e02bbd8a1d587d65
John Morgan <jwjmorgan@gmail.com>
2022-02-10 09:39:09 +0000

    Add fontawesome icons to footer socials

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,19 +1,36 @@
 import React from "react";
 import classes from "./Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
       <div className={classes.socials}>
-        <a href="https://github.com/Johnny-Morgan" target="_blank">
-          <i class="fab fa-github-square"></i>
+        <a
+          href="https://github.com/Johnny-Morgan"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            className={classes.icon}
+          ></FontAwesomeIcon>
         </a>
-        <a href="https://www.linkedin.com/in/john--morgan/" target="_blank">
-          <i class="fab fa-linkedin"></i>
+        <a
+          href="https://www.linkedin.com/in/john--morgan/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            className={classes.icon}
+          ></FontAwesomeIcon>
         </a>
       </div>
       <div className={classes.copyright}>
-        &copy; John Morgan {new Date().getFullYear()}
+        &copy; {new Date().getFullYear()} John Morgan
       </div>
     </footer>
   );
