0a8a10edf6acc6fb6cade9c75f9b0404461d3629
John Morgan <jwjmorgan@gmail.com>
2022-02-10 11:10:01 +0000

    Add Footer component to App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <About />
         <Projects />
         <Contact />
+        <Footer />
       </div>
     </div>
   );
