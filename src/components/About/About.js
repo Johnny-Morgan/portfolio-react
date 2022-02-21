4fe52b41f2c11a267d089b7692d377eeb375f18f
John Morgan <jwjmorgan@gmail.com>
2022-02-21 21:39:59 +0000

    Add social media icons to about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,9 @@
 import React from "react";
 import portraitImage from "./portrait.png";
 import classes from "./About.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const About = () => {
   return (
@@ -8,6 +11,28 @@ const About = () => {
       <div className={classes.container}>
         <div className={classes.about__info}>
           <h2>Hello, I'm John</h2>
+          <div className={classes.about__socials}>
+            <a
+              href="https://github.com/Johnny-Morgan"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                className={classes["about__socials--icon"]}
+              ></FontAwesomeIcon>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/john--morgan/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                className={classes["about__socials--icon"]}
+              ></FontAwesomeIcon>
+            </a>
+          </div>
           <p>
             I'm a full stack software developer. Lorem ipsum dolor sit amet
             consectetur adipisicing elit. Natus distinctio, nesciunt architecto
