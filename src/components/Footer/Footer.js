5382f5a72ba1cb2512b5f826d7797bed8e703bd9
John Morgan <jwjmorgan@gmail.com>
2022-02-22 01:32:44 +0000

    Add Socials component to Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,33 +1,16 @@
 import React from "react";
 import classes from "./Footer.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import Socials from "../Socials/Socials";
 
 const Footer = () => {
   return (
     <footer className={classes.footer}>
       <div className={classes.socials}>
-        <a
-          href="https://github.com/Johnny-Morgan"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon
-            icon={faGithub}
-            className={classes.icon}
-          ></FontAwesomeIcon>
-        </a>
-        <a
-          href="https://www.linkedin.com/in/john--morgan/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            className={classes.icon}
-          ></FontAwesomeIcon>
-        </a>
+        <Socials
+          iconSize="icon--small"
+          iconStyle="icon--white"
+          border={false}
+        />
       </div>
       <div className={classes.copyright}>
         &copy; {new Date().getFullYear()} John Morgan
