6fe04e9cca0097d92fd0547542703a53bfa8d697
John Morgan <jwjmorgan@gmail.com>
2022-02-22 00:18:49 +0000

    Create Socials component

---
+++ src/components/Socials/Socials.js
@@ -0,0 +1,34 @@
+import React from "react";
+import classes from "./Socials.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+const Socials = ({ iconSize, iconStyle }) => {
+  return (
+    <>
+      <a
+        href="https://github.com/Johnny-Morgan"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon
+          icon={faGithub}
+          className={`${classes.icon} ${classes[iconSize]} ${classes[iconStyle]}`}
+        ></FontAwesomeIcon>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/john--morgan/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon
+          icon={faLinkedin}
+          className={`${classes.icon} ${classes[iconSize]} ${classes[iconStyle]}`}
+        ></FontAwesomeIcon>
+      </a>
+    </>
+  );
+};
+
+export default Socials;
