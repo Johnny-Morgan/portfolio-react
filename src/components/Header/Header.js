bfe7e9edd00e90eeed681c04d6ebd69d7d4a80ab
John Morgan <jwjmorgan@gmail.com>
2022-02-22 14:43:34 +0000

    Add Socials component to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import classes from "./Header.module.css";
-
+import Socials from "../Socials/Socials";
 const Header = () => {
   return (
     <header className={classes.header}>
-      <h1 className={classes.header__logo}>JohnMorgan.dev</h1>
+      <h1 className={classes.header__logo}>
+        JohnMorgan.dev
+        <Socials iconSize="icon--large" iconStyle="icon--white" />
+      </h1>
       <Navbar />
     </header>
   );
